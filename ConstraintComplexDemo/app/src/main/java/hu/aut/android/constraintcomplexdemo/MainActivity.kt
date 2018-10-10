f94246552a51c68119aeed917f60c13cfd084dbd
Péter Ekler <peter.ekler@gmail.com>
2018-10-11 01:03:20 +0200

    ContentProvider and ConstraintLayout demos

---
+++ ConstraintComplexDemo/app/src/main/java/hu/aut/android/constraintcomplexdemo/MainActivity.kt
@@ -0,0 +1,13 @@
+package hu.aut.android.constraintcomplexdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.FrameMetrics
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
